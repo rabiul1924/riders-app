aa3489d5750efca41f1ad6b074d90757099ed66f
Md.Rabiul islam <66365150+rabiul1924@users.noreply.github.com>
2021-04-25 23:54:17 +0600

    Delete firebaseConfig.js

--- src/components/Login/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBYzHiy3CyoTZ_oQaxrd2FLi9VaAj1TlpM",
-  authDomain: "test-aef3a.firebaseapp.com",
-  projectId: "test-aef3a",
-  storageBucket: "test-aef3a.appspot.com",
-  messagingSenderId: "616870180234",
-  appId: "1:616870180234:web:909d48069b95753b434473"
-};
-export default firebaseConfig;
